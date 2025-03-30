91a34c3ed7f869de563639698d861ecc66554ff7
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-03-30 23:27:24 +0100

    week three

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,4 @@
 # Create a native segwit address and get the public key from the address.
+SEGWIT=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+PUBKEY=$(bitcoin-cli -regtest getaddressinfo $SEGWIT |jq -r '.pubkey')
+echo $PUBKEY
--- submission/07.sh
+++ submission/07.sh
@@ -1,2 +1,6 @@
 # What is the receiver's address in this partially signed transaction?
-# transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+RAW_PSBT=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+DECODE_PSBT=$(bitcoin-cli -regtest decodepsbt $RAW_PSBT| jq -r '.tx.vout[0].scriptPubkey.address')
+
+echo DECODE_PSBT
